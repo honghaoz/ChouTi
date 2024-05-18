c702b3d736aca6cf43c0441b739db6873c3a26d9
Honghao Zhang <m@honghao.dev>
2024-05-17 21:10:25 -0700

    add Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.8
+
+import PackageDescription
+
+let package = Package(
+  name: "ChouTi",
+  platforms: [
+    .iOS(.v13),
+    .macOS(.v10_15),
+  ],
+  products: [
+    .library(name: "ChouTiTest", targets: ["ChouTiTest"]),
+  ],
+  dependencies: [],
+  targets: [
+    .target(
+      name: "ChouTiTest",
+      dependencies: [],
+      path: "packages/ChouTiTest/Sources"
+    ),
+  ]
+)
