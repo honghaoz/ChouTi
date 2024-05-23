c95bf7a29aafed78e91b473dd2b1603a3052919b
Honghao Zhang <m@honghao.dev>
2024-05-22 21:57:06 -0700

    [scripts] update spm build test scripts

--- scripts/swift-package/build-workspace.sh
+++ scripts/swift-package/build-workspace.sh
@@ -23,38 +23,72 @@ tput-reset() {
 print_help() {
   echo "$(tput-bold)OVERVIEW:$(tput-reset) Build Swift package via a workspace for all platforms."
   echo ""
-  echo "$(tput-bold)Usage:$(tput-reset) $0 --workspace-path <workspace_path> --scheme <scheme_name> --configuration <configuration>"
+  echo "$(tput-bold)Usage:$(tput-reset) $0 --workspace-path <workspace_path> --scheme <scheme_name> --configuration <configuration> --os <iOS macOS visionOS>"
   echo ""
   echo "$(tput-bold)OPTIONS:$(tput-reset)"
   echo "  --workspace-path <workspace_path>  The path to the workspace. Required."
   echo "  --scheme <scheme_name>             The scheme to build. Required."
   echo "  --configuration <configuration>    The build configuration. Optional. Default is Debug."
+  echo "  --os <iOS macOS visionOS>          The list of OS to build for. Optional. Default is 'iOS macOS visionOS'."
   echo "  --help, -h                         Show this help message."
   echo ""
   echo "$(tput-bold)EXAMPLES:$(tput-reset)"
-  echo "  $0 --workspace path/to/Project.xcworkspace --scheme ChouTi --configuration Debug|Release"
+  echo "  $0 --workspace path/to/Project.xcworkspace --scheme ChouTi --configuration Debug|Release --os iOS macOS visionOS"
 }
 
-CONFIGURATION="Debug"
 WORKSPACE_PATH=""
 SCHEME=""
+CONFIGURATION="Debug"
+OS=""
 
 while [[ "$#" -gt 0 ]]; do
   case $1 in
   --workspace-path)
+    if [ -z "$2" ]; then
+      echo "🛑 Missing value for --workspace-path" >&2
+      exit 1
+    fi
     WORKSPACE_PATH="$2"
     shift # past option
     shift # past value
     ;;
   --scheme)
+    if [ -z "$2" ]; then
+      echo "🛑 Missing value for --scheme" >&2
+      exit 1
+    fi
     SCHEME="$2"
     shift # past option
     shift # past value
     ;;
   --configuration)
+    if [ -z "$2" ]; then
+      echo "🛑 Missing value for --configuration" >&2
+      exit 1
+    fi
     CONFIGURATION="$2"
     shift # past option
     shift # past value
+    ;;
+  --os)
+    if [ -z "$2" ]; then
+      echo "🛑 Missing value for --os" >&2
+      exit 1
+    fi
+    # consume a list of arguments
+    OS="$2"
+    shift # past option
+    shift # past value
+    # consume all remaining arguments
+    while [[ "$1" != "--"* ]] && [[ "$#" -gt 0 ]]; do
+      # arguments should be in [iOS macOS visionOS]
+      if [[ "$1" != "iOS" ]] && [[ "$1" != "macOS" ]] && [[ "$1" != "visionOS" ]]; then
+        echo "🛑 Invalid OS: $1" >&2
+        exit 1
+      fi
+      OS="$OS $1"
+      shift
+    done
     ;;
   --help | -h)
     print_help
@@ -97,6 +131,11 @@ if [ -z "$SCHEME" ]; then
   exit 1
 fi
 
+# ensure the OS is provided
+if [ -z "$OS" ]; then
+  OS="iOS macOS visionOS"
+fi
+
 # ensure workspace path exists
 if [ ! -d "$WORKSPACE_PATH" ]; then
   echo "🛑 Workspace not found: $WORKSPACE_PATH"
@@ -113,7 +152,7 @@ WORKSPACE=$(basename "$WORKSPACE_PATH")
 
 cd "$WORKSPACE_DIR" || exit 1
 
-echo "🚀 Build workspace: $(tput-cyan)$WORKSPACE$(tput-reset), scheme: $(tput-cyan)$SCHEME$(tput-reset), configuration: $(tput-cyan)$CONFIGURATION$(tput-reset)"
+echo "🚀 Build workspace: $(tput-cyan)$WORKSPACE$(tput-reset), scheme: $(tput-cyan)$SCHEME$(tput-reset), configuration: $(tput-cyan)$CONFIGURATION$(tput-reset), os: $(tput-cyan)$OS$(tput-reset)"
 
 echo "$(tput-bold)Swift Version:$(tput-reset) $(swift --version)"
 echo "$(tput-bold)Xcode Version:$(tput-reset) $(xcodebuild -version)"
@@ -124,30 +163,37 @@ echo "ℹ️  $WORKSPACE, Scheme: $SCHEME, available destinations:"
 xcodebuild -workspace "$WORKSPACE" -scheme "$SCHEME" -showdestinations
 
 # For macOS
-echo "➡️  [1/2] Building for macOS ($CONFIGURATION)..."
-SWIFT_BUILD_CONFIG="debug"
-if [ "$CONFIGURATION" == "Release" ]; then
-  SWIFT_BUILD_CONFIG="release"
+if [[ "$OS" == *"macOS"* ]]; then
+  echo ""
+  echo "➡️  Building for macOS ($CONFIGURATION)..."
+  SWIFT_BUILD_CONFIG="debug"
+  if [ "$CONFIGURATION" == "Release" ]; then
+    SWIFT_BUILD_CONFIG="release"
+  fi
+  set -o pipefail && swift build -c "$SWIFT_BUILD_CONFIG" || ERROR_CODE=$?
 fi
-set -o pipefail && swift build -c "$SWIFT_BUILD_CONFIG" || ERROR_CODE=$?
 
 # For iOS
-echo ""
-echo "➡️  [2/2] Building for iOS ($CONFIGURATION)..."
-SIMULATOR_NAME=$(xcrun simctl list devices available | grep 'iPhone' | grep -Eo 'iPhone \d+' | sort -t ' ' -k 2 -nr | head -1)
-PLATFORM="iOS Simulator"
-DESTINATION="platform=$PLATFORM,name=$SIMULATOR_NAME"
-# DESTINATION="generic/platform=iOS"
-set -o pipefail && xcodebuild build -workspace "$WORKSPACE" -scheme "$SCHEME" -destination "$DESTINATION" -configuration "$CONFIGURATION" | "$REPO_ROOT"/bin/xcbeautify || ERROR_CODE=$?
+if [[ "$OS" == *"iOS"* ]]; then
+  echo ""
+  echo "➡️  Building for iOS ($CONFIGURATION)..."
+  SIMULATOR_NAME=$(xcrun simctl list devices available | grep 'iPhone' | grep -Eo 'iPhone \d+' | sort -t ' ' -k 2 -nr | head -1)
+  PLATFORM="iOS Simulator"
+  DESTINATION="platform=$PLATFORM,name=$SIMULATOR_NAME"
+  # DESTINATION="generic/platform=iOS"
+  set -o pipefail && xcodebuild build -workspace "$WORKSPACE" -scheme "$SCHEME" -destination "$DESTINATION" -configuration "$CONFIGURATION" | "$REPO_ROOT"/bin/xcbeautify || ERROR_CODE=$?
+fi
 
 # For visionOS
-echo ""
-echo "➡️  [3/3] Building for visionOS ($CONFIGURATION)..."
-SIMULATOR_NAME=$(xcrun simctl list devices available | grep "Apple Vision" | head -n 1 | awk -F'(' '{print $1}' | xargs)
-PLATFORM="visionOS Simulator"
-DESTINATION="platform=$PLATFORM,name=$SIMULATOR_NAME"
-# DESTINATION="generic/platform=visionOS"
-set -o pipefail && xcodebuild build -workspace "$WORKSPACE" -scheme "$SCHEME" -destination "$DESTINATION" -configuration "$CONFIGURATION" | "$REPO_ROOT"/bin/xcbeautify || ERROR_CODE=$?
+if [[ "$OS" == *"visionOS"* ]]; then
+  echo ""
+  echo "➡️  Building for visionOS ($CONFIGURATION)..."
+  SIMULATOR_NAME=$(xcrun simctl list devices available | grep "Apple Vision" | head -n 1 | awk -F'(' '{print $1}' | xargs)
+  PLATFORM="visionOS Simulator"
+  DESTINATION="platform=$PLATFORM,name=$SIMULATOR_NAME"
+  # DESTINATION="generic/platform=visionOS"
+  set -o pipefail && xcodebuild build -workspace "$WORKSPACE" -scheme "$SCHEME" -destination "$DESTINATION" -configuration "$CONFIGURATION" | "$REPO_ROOT"/bin/xcbeautify || ERROR_CODE=$?
+fi
 
 if [ $ERROR_CODE -ne 0 ]; then
   echo "🛑 Build failed."
--- scripts/swift-package/test-workspace.sh
+++ scripts/swift-package/test-workspace.sh
@@ -23,11 +23,12 @@ tput-reset() {
 print_help() {
   echo "$(tput-bold)OVERVIEW:$(tput-reset) Build Swift package via a workspace for all platforms."
   echo ""
-  echo "$(tput-bold)Usage:$(tput-reset) $0 --workspace-path <workspace_path> --scheme <scheme_name>"
+  echo "$(tput-bold)Usage:$(tput-reset) $0 --workspace-path <workspace_path> --scheme <scheme_name> --os <iOS macOS visionOS>"
   echo ""
   echo "$(tput-bold)OPTIONS:$(tput-reset)"
   echo "  --workspace-path <workspace_path>  The path to the workspace. Required."
   echo "  --scheme <scheme_name>             The scheme to build. Required."
+  echo "  --os <iOS macOS visionOS>          The list of OS to build for. Optional. Default is 'iOS macOS visionOS'."
   echo "  --help, -h                         Show this help message."
   echo ""
   echo "$(tput-bold)EXAMPLES:$(tput-reset)"
@@ -36,18 +37,47 @@ print_help() {
 
 WORKSPACE_PATH=""
 SCHEME=""
+OS=""
 
 while [[ "$#" -gt 0 ]]; do
   case $1 in
   --workspace-path)
+    if [ -z "$2" ]; then
+      echo "🛑 Missing value for --workspace-path" >&2
+      exit 1
+    fi
     WORKSPACE_PATH="$2"
     shift # past option
     shift # past value
     ;;
   --scheme)
+    if [ -z "$2" ]; then
+      echo "🛑 Missing value for --scheme" >&2
+      exit 1
+    fi
     SCHEME="$2"
     shift # past option
     shift # past value
+    ;;
+  --os)
+    if [ -z "$2" ]; then
+      echo "🛑 Missing value for --os" >&2
+      exit 1
+    fi
+    # consume a list of arguments
+    OS="$2"
+    shift # past option
+    shift # past value
+    # consume all remaining arguments
+    while [[ "$1" != "--"* ]] && [[ "$#" -gt 0 ]]; do
+      # arguments should be in [iOS macOS visionOS]
+      if [[ "$1" != "iOS" ]] && [[ "$1" != "macOS" ]] && [[ "$1" != "visionOS" ]]; then
+        echo "🛑 Invalid OS: $1" >&2
+        exit 1
+      fi
+      OS="$OS $1"
+      shift
+    done
     ;;
   --help | -h)
     print_help
@@ -82,6 +112,11 @@ if [ -z "$SCHEME" ]; then
   exit 1
 fi
 
+# ensure the OS is provided
+if [ -z "$OS" ]; then
+  OS="iOS macOS visionOS"
+fi
+
 # ensure workspace path exists
 if [ ! -d "$WORKSPACE_PATH" ]; then
   echo "🛑 Workspace not found: $WORKSPACE_PATH"
@@ -98,7 +133,7 @@ WORKSPACE=$(basename "$WORKSPACE_PATH")
 
 cd "$WORKSPACE_DIR" || exit 1
 
-echo "🎯 Test workspace: $(tput-cyan)$WORKSPACE$(tput-reset), scheme: $(tput-cyan)$SCHEME$(tput-reset)"
+echo "🎯 Test workspace: $(tput-cyan)$WORKSPACE$(tput-reset), scheme: $(tput-cyan)$SCHEME$(tput-reset), os: $(tput-cyan)$OS$(tput-reset)"
 
 echo "$(tput-bold)Swift Version:$(tput-reset) $(swift --version)"
 echo "$(tput-bold)Xcode Version:$(tput-reset) $(xcodebuild -version)"
@@ -109,31 +144,37 @@ echo "ℹ️  $WORKSPACE, Scheme: $SCHEME, available destinations:"
 xcodebuild -workspace "$WORKSPACE" -scheme "$SCHEME" -showdestinations
 
 # For macOS
-echo "➡️  [1/2] Running tests for macOS..."
-# use swift test:
-# pass 'TEST' compiler flag so the code can use "#if TEST" to conditionally 'import XCTest'.
-set -o pipefail && swift test -Xswiftc -DTEST | "$REPO_ROOT"/bin/xcbeautify || ERROR_CODE=$?
-# use xcodebuild:
-# DESTINATION="platform=macOS,name=Any Mac"
-# set -o pipefail && xcodebuild test -workspace "$WORKSPACE" -scheme "$SCHEME" -destination "$DESTINATION" | "$REPO_ROOT"/bin/xcbeautify || ERROR_CODE=$?
+if [[ "$OS" == *"macOS"* ]]; then
+  echo "➡️  Running tests for macOS..."
+  # use swift test:
+  # pass 'TEST' compiler flag so the code can use "#if TEST" to conditionally 'import XCTest'.
+  set -o pipefail && swift test -Xswiftc -DTEST | "$REPO_ROOT"/bin/xcbeautify || ERROR_CODE=$?
+  # use xcodebuild:
+  # DESTINATION="platform=macOS,name=Any Mac"
+  # set -o pipefail && xcodebuild test -workspace "$WORKSPACE" -scheme "$SCHEME" -destination "$DESTINATION" | "$REPO_ROOT"/bin/xcbeautify || ERROR_CODE=$?
+fi
 
 # For iOS
-echo ""
-echo "➡️  [2/2] Running tests for iOS..."
-SIMULATOR_NAME=$(xcrun simctl list devices available | grep 'iPhone' | grep -Eo 'iPhone \d+' | sort -t ' ' -k 2 -nr | head -1)
-PLATFORM="iOS Simulator"
-DESTINATION="platform=$PLATFORM,name=$SIMULATOR_NAME"
-echo "Running tests for $SIMULATOR_NAME..."
-set -o pipefail && xcodebuild test -workspace "$WORKSPACE" -scheme "$SCHEME" -destination "$DESTINATION" | "$REPO_ROOT"/bin/xcbeautify || ERROR_CODE=$?
+if [[ "$OS" == *"iOS"* ]]; then
+  echo ""
+  echo "➡️  Running tests for iOS..."
+  SIMULATOR_NAME=$(xcrun simctl list devices available | grep 'iPhone' | grep -Eo 'iPhone \d+' | sort -t ' ' -k 2 -nr | head -1)
+  PLATFORM="iOS Simulator"
+  DESTINATION="platform=$PLATFORM,name=$SIMULATOR_NAME"
+  echo "Running tests for $SIMULATOR_NAME..."
+  set -o pipefail && xcodebuild test -workspace "$WORKSPACE" -scheme "$SCHEME" -destination "$DESTINATION" | "$REPO_ROOT"/bin/xcbeautify || ERROR_CODE=$?
+fi
 
 # For visionOS
-echo ""
-echo "➡️  [3/3] Running tests for visionOS..."
-SIMULATOR_NAME=$(xcrun simctl list devices available | grep "Apple Vision" | head -n 1 | awk -F'(' '{print $1}' | xargs)
-PLATFORM="visionOS Simulator"
-DESTINATION="platform=$PLATFORM,name=$SIMULATOR_NAME"
-echo "Running tests for $SIMULATOR_NAME..."
-set -o pipefail && xcodebuild test -workspace "$WORKSPACE" -scheme "$SCHEME" -destination "$DESTINATION" | "$REPO_ROOT"/bin/xcbeautify || ERROR_CODE=$?
+if [[ "$OS" == *"visionOS"* ]]; then
+  echo ""
+  echo "➡️  Running tests for visionOS..."
+  SIMULATOR_NAME=$(xcrun simctl list devices available | grep "Apple Vision" | head -n 1 | awk -F'(' '{print $1}' | xargs)
+  PLATFORM="visionOS Simulator"
+  DESTINATION="platform=$PLATFORM,name=$SIMULATOR_NAME"
+  echo "Running tests for $SIMULATOR_NAME..."
+  set -o pipefail && xcodebuild test -workspace "$WORKSPACE" -scheme "$SCHEME" -destination "$DESTINATION" | "$REPO_ROOT"/bin/xcbeautify || ERROR_CODE=$?
+fi
 
 if [ $ERROR_CODE -ne 0 ]; then
   echo "🛑 Tests failed."
