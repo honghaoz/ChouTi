4c9d958b091d9966c5e22d3a0eeacea569b0026b
Honghao Zhang <m@honghao.dev>
2024-05-28 10:01:07 -0700

    auto import XCTest

---
+++ packages/ChouTiTest/Sources/ChouTiTest/Export.swift
@@ -0,0 +1,9 @@
+//
+//  Export.swift
+//
+//  Created by Honghao Zhang on 5/28/24.
+//  Copyright © 2024 ChouTi. All rights reserved.
+//
+
+/// import `ChouTiTest` will import `XCTest`.
+@_exported import XCTest
