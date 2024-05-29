af10c7e987ffb22c3115906f720363321a7d6f26
Honghao Zhang <m@honghao.dev>
2024-05-28 23:43:40 -0700

    [MachTimeId] MachTimeId.id() to generate absolute unique id
    since MachTimeId.id() is usually used for a unique id scenarios, just make it guaranteed unique

--- packages/ChouTi/Sources/ChouTi/MachTime/MachTimeId.swift
+++ packages/ChouTi/Sources/ChouTi/MachTime/MachTimeId.swift
@@ -14,50 +14,38 @@ public typealias MachTimeId = UInt64
 
 public extension MachTimeId {
 
-  /// Make a new id based on mach absolute time.
+  private static var lastId: MachTimeId = 0
+  private static let lastIdLock = UnfairLock()
+
+  /// Generates a new unique id based on mach absolute time.
   ///
   /// This is generally the fastest way to get a unique id within the same app run.
   ///
-  /// - Warning: For consecutive calls, the returned ids maybe the same. For this case, you may need to use a variable to store the last id to guarantee the uniqueness. For example:
-  /// ```swift
-  /// var lastId: MachTimeId = 0
+  /// This method generates a unique id in a thread-safe way.
   ///
-  /// for _ in 1...100 {
-  ///   var newId = MachTimeId.id()
-  ///   if newId <= lastId {
-  ///     newId = lastId + 1
-  ///   }
-  ///   lastId = newId
-  ///   print(newId)
-  /// }
-  /// ```
-  @inlinable
-  @inline(__always)
+  /// - Returns: A unique id.
   static func id() -> MachTimeId {
-    mach_absolute_time()
+    var newId = mach_absolute_time()
+    lastIdLock.withLock {
+      if newId <= lastId {
+        newId = lastId + 1
+      }
+      lastId = newId
+    }
+    return newId
   }
 
   /// Make a new id string based on mach absolute time.
   ///
   /// This is generally the fastest way to get a unique id within the same app run.
   ///
-  /// - Warning: For consecutive calls, the returned ids maybe the same. For this case, you may need to use a variable to store the last id to guarantee the uniqueness. For example:
-  /// ```swift
-  /// var lastId: String = ""
+  /// This method generates a unique id in a thread-safe way.
   ///
-  /// for _ in 1...100 {
-  ///   var newId = MachTimeId.idString()
-  ///   if newId <= lastId {
-  ///     newId = lastId + "1"
-  ///   }
-  ///   lastId = newId
-  ///   print(newId)
-  /// }
-  /// ```
+  /// - Returns: A unique id string.
   @inlinable
   @inline(__always)
   static func idString() -> String {
-    String(mach_absolute_time())
+    String(MachTimeId.id())
   }
 }
 
--- packages/ChouTi/Tests/ChouTiTests/MachTime/MachTimeIdTests.swift
+++ packages/ChouTi/Tests/ChouTiTests/MachTime/MachTimeIdTests.swift
@@ -6,49 +6,126 @@
 //
 
 import Foundation
-import XCTest
+
 import ChouTiTest
 
 import ChouTi
 
 final class MachTimeIdTests: XCTestCase {
 
-  func testMachTimeId_canConflict() {
-    var isConflicted: Bool = false
-    for _ in 1 ... 100 {
+  func testMachTimeId_noConflict() {
+    for _ in 1 ... 10000 {
       let id1 = MachTimeId.id()
       let id2 = MachTimeId.id()
 
       if id1 == id2 {
-        isConflicted = true
+        expect(id1) != id2
         break
       }
     }
-
-    expect(isConflicted) == true // it's totally possible to have conflicts for consecutive calls
   }
 
-  func testMachTimeId_noConflict() {
-    let id1 = MachTimeId.id()
-    let a = UUID().uuidString
-    _ = a
-    let id2 = MachTimeId.id()
-    expect(id1) != id2
+  func testMachTimeId_noConflict_multiThread() {
+    let group = DispatchGroup()
+    let queue = DispatchQueue(label: "testMachTimeId_noConflict_multiThread", attributes: .concurrent)
+
+    for _ in 1 ... 10000 {
+      group.enter()
+      queue.async {
+        let id1 = MachTimeId.id()
+        let id2 = MachTimeId.id()
+
+        if id1 == id2 {
+          expect(id1) != id2
+        }
+
+        group.leave()
+      }
+    }
+
+    group.wait()
+  }
+
+  func testMachTimeId_noConflict_multiThread2() {
+    // use GCD concurrentPerform
+    let count = 10000
+    var ids = [MachTimeId](repeating: 0, count: count)
+    DispatchQueue.concurrentPerform(iterations: count) { i in
+      ids[i] = MachTimeId.id()
+    }
+
+    // check conflict
+    let set = Set(ids)
+    expect(set.count) == count
   }
 
   func testMachTimeIdString_noConflict() {
-    let id1 = MachTimeId.idString()
-    let a = UUID().uuidString
-    _ = a
-    let id2 = MachTimeId.idString()
-    expect(id1) != id2
+    for _ in 1 ... 10000 {
+      let id1 = MachTimeId.idString()
+      let id2 = MachTimeId.idString()
+
+      if id1 == id2 {
+        expect(id1) != id2
+        break
+      }
+    }
   }
 
-  func testStringMachTimeId() {
-    let id1 = String.machTimeId()
-    let a = UUID().uuidString
-    _ = a
-    let id2 = String.machTimeId()
-    expect(id1) != id2
+  func testMachTimeIdString_noConflict_multiThread() {
+    let group = DispatchGroup()
+    let queue = DispatchQueue(label: "testMachTimeIdString_noConflict_multiThread", attributes: .concurrent)
+
+    for _ in 1 ... 10000 {
+      group.enter()
+      queue.async {
+        let id1 = MachTimeId.idString()
+        let id2 = MachTimeId.idString()
+
+        if id1 == id2 {
+          expect(id1) != id2
+        }
+
+        group.leave()
+      }
+    }
+
+    group.wait()
+  }
+
+  func testMachTimeIdString_noConflict_multiThread2() {
+    // use GCD concurrentPerform
+    let count = 10000
+    var ids = [String](repeating: "", count: count)
+    DispatchQueue.concurrentPerform(iterations: count) { i in
+      ids[i] = MachTimeId.idString()
+    }
+
+    // check conflict
+    let set = Set(ids)
+    expect(set.count) == count
+  }
+
+  // func test_performance() {
+  func performance() {
+    PerformanceMeasurer.measure(tag: "mach_absolute_time()", repeatCount: Int(1e6)) {
+      _ = mach_absolute_time()
+    }
+
+    PerformanceMeasurer.measure(tag: "MachTimeId.id()", repeatCount: Int(1e6)) {
+      _ = MachTimeId.id()
+    }
+
+    PerformanceMeasurer.measure(tag: "UUID()", repeatCount: Int(1e6)) {
+      _ = UUID()
+    }
+
+    PerformanceMeasurer.measure(tag: "UUID().uuidString", repeatCount: Int(1e6)) {
+      _ = UUID().uuidString
+    }
+
+    // [mach_absolute_time()] Elapsed time: 0.23118008332676254
+    // [MachTimeId.id()] Elapsed time: 0.2687903333280701
+    // [UUID()] Elapsed time: 0.44729162499425
+    // [UUID().uuidString] Elapsed time: 0.527294500003336
   }
 }
